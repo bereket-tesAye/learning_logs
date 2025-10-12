7adf8b41aeab98f4afb4c677f37227413362145e
Bereket Tesfaye <tesfayebeks@gmail.com>
2025-10-13 00:18:39 +0300

    edit entry feature added

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -5,7 +5,7 @@ from .forms import TopicForm
 from .forms import EntryForm
 
 from django.shortcuts import render
-from .models import Topic
+from .models import Topic, Entry
 
 # Create your views here.
 def index(request):
@@ -48,3 +48,18 @@ def new_entry(request, topic_id):
                                         args=[topic_id]))
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    entry = Entry.objects.get(id = entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance = entry, data = request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args = [topic.id]))
+        
+    context = {'entry':entry, 'topic': topic, 'form':form }
+    return render(request, 'learning_logs/edit_entry.html', context)
